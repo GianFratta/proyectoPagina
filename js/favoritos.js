5e8f17193e8b44be76435ca4d274134cffd026d2
GianFratta <gfratta@udesa.edu.ar>
2022-11-14 20:17:47 -0300

    la concha de mi hermana

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,5 +1,36 @@
 
-let recuperoStorage = localStorage.getItem('peliculasFav')
-let peliculas = JSON.parse(recuperoStorage);
-console.log(peliculas);
+let favoritos = []
 
+let recuperStorage = localStorage.getItem('pelisFavs')
+
+
+if(recuperStorage !== null){
+    favoritos = JSON.parse(recuperStorage)
+}
+ 
+let estrella = document.querySelector('.palabraRute');
+
+/* #################################################### */
+
+if(favoritos.includes(id)){
+    estrella.innerText = <i class="fa-regular fa-star"></i>
+}
+ 
+estrella.addEventListener('click', function(){
+
+    if(favoritos.includes(id)){
+       let indiceDelPersonaje = favoritos.indexOf(id);
+       favoritos.splice(indiceDelPersonaje, 1)
+       estrella.innerText = 'Agregar a favoritos';
+ 
+    } else {
+          favoritos.push(id);
+          estrella.innerText = <i class="fa-solid fa-star"></i>;
+    }
+ 
+    let favsToString = JSON.stringify(favoritos)
+    localStorage.setItem('pelisFavs',favsToString)
+    
+    console.log(localStorage);
+ 
+ })
